76240d4277cd43c45bd782725fe774cf32f30274
MKC <59623220+MKC-MKC@users.noreply.github.com>
2020-05-23 00:32:29 +0300

    Add form in empty page

--- index/error.php
+++ index/error.php
@@ -1,3 +1,56 @@
+<!-- create -->
+<div class="bloc none bloc-fill-screen tc-white l-bloc" id="create">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3 class="text-lg-center mx-auto d-block text-md-center text-sm-center btn-resize-mode text-center mg-clear h3-4-style tc-white">Your link doesn't contain useful data</h3>
+                <h3 class="text-lg-center mx-auto d-block text-md-center text-sm-center btn-resize-mode text-center h3-create-style tc-white">Create link with our form</h3>
+                <div class="row row-style align-items-center">
+                    <div class="col-md-8 offset-md-2 align-self-center col-sm-10 offset-sm-1 offset-lg-3 col-lg-6">
+                        <form id="get_request" action="" method="GET">
+
+                            <div class="form-group">
+                                <label for="input" class="label-style">Input .plist URL here:</label>
+                                <input id="input" class="form-control rback" required name="url" />
+                            </div>
+
+                            <div class="text-center"><button class="bloc-button btn btn-style btn-sm btn-d rback" type="submit">Create QR</button></div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container fill-bloc-bottom-edge">
+        <div class="row align-items-center">
+            <div class="col-12 col-lg-4 col-sm-12 align-self-center col-md-4">
+                <div class="blockquote text-md-center text-sm-center text-center container-div-style">
+                    <p class="btn-resize-mode p-style mx-auto d-block text-center">Open source:&nbsp;<a class="ltc-white" href="https://github.com/MKC-MKC/itms-services-app-installer">GitHub</a></p>
+                </div>
+            </div>
+            <div class="col-12 col-lg-4 col-sm-12 align-self-center col-md-4">
+                <div class="blockquote text-md-center text-sm-center text-center container-div-error-style">
+                    <p class="text-lg-center mx-auto d-block btn-resize-mode text-center p-style">
+                        Idea:&nbsp;<a class="ltc-white" href="https://linkbridge.ru/deep/?URL=tg://resolve?domain=user1man" rel="nofollow noopener noreferrer" target="_blank">@user1man</a>
+                    </p>
+                </div>
+            </div>
+            <div class="col-12 col-lg-4 col-sm-12 align-self-center col-md-4">
+                <div class="blockquote text-md-center text-sm-center text-center">
+                    <p class="text-lg-center mx-auto d-block btn-resize-mode p-style text-md-center">
+                        Author:&nbsp;<a class="ltc-white" href="https://linkbridge.ru/deep/?URL=tg://resolve?domain=MiKillCrafter" rel="nofollow noopener noreferrer" target="_blank">@MiKillCrafter</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- create END -->
+
+<?php
+/*
 <!-- error -->
 <div class="bloc l-bloc none bloc-fill-screen tc-red" id="error">
 
@@ -48,3 +101,4 @@
 
 </div>
 <!-- error END -->
+*/
